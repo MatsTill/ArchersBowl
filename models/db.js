4b9d75df7702716d9eff3c640e68a595cf95ab98
MatsTill <117838310+MatsTill@users.noreply.github.com>
2023-04-19 18:59:17 +0800

    Update db.js

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ dotenv.config();
 import mongoose from 'mongoose';
 
 
-const url = 'mongodb://localhost:27017/ccapdev-mp';
+const url = 'mongodb://localhost:27017/ArchersBowl';
 
 const options = {
     useUnifiedTopology: true,
